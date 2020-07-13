8e8c7e30da2a551a0efdda6ce547d9bb97cdfd36
Azarakhsh Keipour <keipour@gmail.com>
2020-07-13 19:06:10 -0400

    Omni Pos-Ctrl: Fixed attitude reading and thrust projected on current/commanded axes based on input

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -122,6 +122,7 @@ private:
 	uORB::Subscription _vehicle_land_detected_sub{ORB_ID(vehicle_land_detected)};	/**< vehicle land detected subscription */
 	uORB::Subscription _control_mode_sub{ORB_ID(vehicle_control_mode)};		/**< vehicle control mode subscription */
 	uORB::Subscription _parameter_update_sub{ORB_ID(parameter_update)};		/**< notification of parameter updates */
+	uORB::Subscription _att_sub{ORB_ID(vehicle_attitude)};				/**< vehicle attitude */
 	uORB::Subscription _home_pos_sub{ORB_ID(home_position)}; 			/**< home position */
 	uORB::Subscription _hover_thrust_estimate_sub{ORB_ID(hover_thrust_estimate)};
 
@@ -412,6 +413,7 @@ MulticopterPositionControl::poll_subscriptions()
 	_vehicle_land_detected_sub.update(&_vehicle_land_detected);
 	_control_mode_sub.update(&_control_mode);
 	_home_pos_sub.update(&_home_pos);
+	_att_sub.update(&att);
 
 	if (_param_mpc_use_hte.get()) {
 		hover_thrust_estimate_s hte;
