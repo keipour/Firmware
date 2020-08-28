PX4/PX4-Autopilot

Bug Fix(es): 3D-thrust ranges fixed to -1..1 and manual z maps to negative z-thrust now
- The range for 3D-thrust in the 6-DOF multirotor mixer is changed to -1 to 1 now (fixed from 0 to 1)
- The Z thrust in the 6-DOF multirotor mixer is mapped to the Z-thrust command now (fixed from thrust command)
- The manual Z command in the rate controller maps to the negative Z-thrust (fixed from positive Z)
- The variable _thrust_body_sp in the rate controller renamed to _thrust_sp to be compatible with the older variable removed in the last commit
- The code tested in TakeOff, Manual, Hold, Position and Land modes on both tilted hex and iris

Accepting and working with 3D thrust commands now

src/modules/mc_rate_control/MulticopterRateControl.cpp
@@ -213,18 +213,18 @@ MulticopterRateControl::Run()
 					math::superexpo(_manual_control_sp.r, _param_mc_acro_expo_y.get(), _param_mc_acro_supexpoy.get())};
 
 				_rates_sp = man_rate_sp.emult(_acro_rate_max);
-				_thrust_body_sp(0) = _manual_control_sp.x;
-				_thrust_body_sp(1) = _manual_control_sp.y;
-				_thrust_body_sp(2) = _manual_control_sp.z;
+				_thrust_sp(0) = _manual_control_sp.x;
+				_thrust_sp(1) = _manual_control_sp.y;
+				_thrust_sp(2) = -_manual_control_sp.z;
 
 				// publish rate setpoint
 				vehicle_rates_setpoint_s v_rates_sp{};
 				v_rates_sp.roll = _rates_sp(0);
 				v_rates_sp.pitch = _rates_sp(1);
 				v_rates_sp.yaw = _rates_sp(2);
-				v_rates_sp.thrust_body[0] = _thrust_body_sp(0);
-				v_rates_sp.thrust_body[1] = _thrust_body_sp(1);
-				v_rates_sp.thrust_body[2] = _thrust_body_sp(2);
+				v_rates_sp.thrust_body[0] = _thrust_sp(0);
+				v_rates_sp.thrust_body[1] = _thrust_sp(1);
+				v_rates_sp.thrust_body[2] = _thrust_sp(2);
 				v_rates_sp.timestamp = hrt_absolute_time();
 
 				_v_rates_sp_pub.publish(v_rates_sp);
@@ -238,9 +238,9 @@ MulticopterRateControl::Run()
 				_rates_sp(0) = v_rates_sp.roll;
 				_rates_sp(1) = v_rates_sp.pitch;
 				_rates_sp(2) = v_rates_sp.yaw;
-				_thrust_body_sp(0) = v_rates_sp.thrust_body[0];
-				_thrust_body_sp(1) = v_rates_sp.thrust_body[1];
-				_thrust_body_sp(2) = v_rates_sp.thrust_body[2];
+				_thrust_sp(0) = v_rates_sp.thrust_body[0];
+				_thrust_sp(1) = v_rates_sp.thrust_body[1];
+				_thrust_sp(2) = v_rates_sp.thrust_body[2];
 			}
 		}
 
@@ -292,11 +292,11 @@ MulticopterRateControl::Run()
 			actuators.control[actuator_controls_s::INDEX_ROLL] = PX4_ISFINITE(att_control(0)) ? att_control(0) : 0.0f;
 			actuators.control[actuator_controls_s::INDEX_PITCH] = PX4_ISFINITE(att_control(1)) ? att_control(1) : 0.0f;
 			actuators.control[actuator_controls_s::INDEX_YAW] = PX4_ISFINITE(att_control(2)) ? att_control(2) : 0.0f;
-			actuators.control[actuator_controls_s::INDEX_THROTTLE] = PX4_ISFINITE(_thrust_body_sp(2)) ? -_thrust_body_sp(2) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_THROTTLE] = PX4_ISFINITE(_thrust_sp(2)) ? -_thrust_sp(2) : 0.0f;
 			actuators.control[actuator_controls_s::INDEX_LANDING_GEAR] = (float)_landing_gear.landing_gear;
-			actuators.control[actuator_controls_s::INDEX_X_THRUST] = (PX4_ISFINITE(_thrust_body_sp(0))) ? _thrust_body_sp(0) : 0.0f;
-			actuators.control[actuator_controls_s::INDEX_Y_THRUST] = (PX4_ISFINITE(_thrust_body_sp(1))) ? _thrust_body_sp(1) : 0.0f;
-			actuators.control[actuator_controls_s::INDEX_Z_THRUST] = (PX4_ISFINITE(_thrust_body_sp(2))) ? _thrust_body_sp(2) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_X_THRUST] = (PX4_ISFINITE(_thrust_sp(0))) ? _thrust_sp(0) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_Y_THRUST] = (PX4_ISFINITE(_thrust_sp(1))) ? _thrust_sp(1) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_Z_THRUST] = (PX4_ISFINITE(_thrust_sp(2))) ? _thrust_sp(2) : 0.0f;
 			actuators.timestamp_sample = angular_velocity.timestamp_sample;
 
 			// scale effort by battery status if enabled

keipour/Firmware

src/modules/mc_rate_control/MulticopterRateControl.cpp
@@ -217,18 +217,18 @@ MulticopterRateControl::Run()
 					math::superexpo(_manual_control_setpoint.r, _param_mc_acro_expo_y.get(), _param_mc_acro_supexpoy.get())};
 
 				_rates_sp = man_rate_sp.emult(_acro_rate_max);
-				_thrust_body_sp(0) = _manual_control_setpoint.x;
-				_thrust_body_sp(1) = _manual_control_setpoint.y;
-				_thrust_body_sp(2) = _manual_control_setpoint.z;
+				_thrust_sp(0) = _manual_control_setpoint.x;
+				_thrust_sp(1) = _manual_control_setpoint.y;
+				_thrust_sp(2) = -_manual_control_setpoint.z;
 
 				// publish rate setpoint
 				vehicle_rates_setpoint_s v_rates_sp{};
 				v_rates_sp.roll = _rates_sp(0);
 				v_rates_sp.pitch = _rates_sp(1);
 				v_rates_sp.yaw = _rates_sp(2);
-				v_rates_sp.thrust_body[0] = _thrust_body_sp(0);
-				v_rates_sp.thrust_body[1] = _thrust_body_sp(1);
-				v_rates_sp.thrust_body[2] = _thrust_body_sp(2);
+				v_rates_sp.thrust_body[0] = _thrust_sp(0);
+				v_rates_sp.thrust_body[1] = _thrust_sp(1);
+				v_rates_sp.thrust_body[2] = _thrust_sp(2);
 				v_rates_sp.timestamp = hrt_absolute_time();
 
 				_v_rates_sp_pub.publish(v_rates_sp);
@@ -242,9 +242,9 @@ MulticopterRateControl::Run()
 				_rates_sp(0) = v_rates_sp.roll;
 				_rates_sp(1) = v_rates_sp.pitch;
 				_rates_sp(2) = v_rates_sp.yaw;
-				_thrust_body_sp(0) = v_rates_sp.thrust_body[0];
-				_thrust_body_sp(1) = v_rates_sp.thrust_body[1];
-				_thrust_body_sp(2) = v_rates_sp.thrust_body[2];			}
+				_thrust_sp(0) = v_rates_sp.thrust_body[0];
+				_thrust_sp(1) = v_rates_sp.thrust_body[1];
+				_thrust_sp(2) = v_rates_sp.thrust_body[2];			}
 		}
 
 		// run the rate controller
@@ -281,11 +281,11 @@ MulticopterRateControl::Run()
 			actuators.control[actuator_controls_s::INDEX_ROLL] = PX4_ISFINITE(att_control(0)) ? att_control(0) : 0.0f;
 			actuators.control[actuator_controls_s::INDEX_PITCH] = PX4_ISFINITE(att_control(1)) ? att_control(1) : 0.0f;
 			actuators.control[actuator_controls_s::INDEX_YAW] = PX4_ISFINITE(att_control(2)) ? att_control(2) : 0.0f;
-			actuators.control[actuator_controls_s::INDEX_THROTTLE] = PX4_ISFINITE(_thrust_body_sp(2)) ? -_thrust_body_sp(2) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_THROTTLE] = PX4_ISFINITE(_thrust_sp(2)) ? -_thrust_sp(2) : 0.0f;
 			actuators.control[actuator_controls_s::INDEX_LANDING_GEAR] = (float)_landing_gear.landing_gear;
-			actuators.control[actuator_controls_s::INDEX_X_THRUST] = (PX4_ISFINITE(_thrust_body_sp(0))) ? _thrust_body_sp(0) : 0.0f;
-			actuators.control[actuator_controls_s::INDEX_Y_THRUST] = (PX4_ISFINITE(_thrust_body_sp(1))) ? _thrust_body_sp(1) : 0.0f;
-			actuators.control[actuator_controls_s::INDEX_Z_THRUST] = (PX4_ISFINITE(_thrust_body_sp(2))) ? _thrust_body_sp(2) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_X_THRUST] = (PX4_ISFINITE(_thrust_sp(0))) ? _thrust_sp(0) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_Y_THRUST] = (PX4_ISFINITE(_thrust_sp(1))) ? _thrust_sp(1) : 0.0f;
+			actuators.control[actuator_controls_s::INDEX_Z_THRUST] = (PX4_ISFINITE(_thrust_sp(2))) ? _thrust_sp(2) : 0.0f;
 			actuators.timestamp_sample = angular_velocity.timestamp_sample;
 
 			// scale effort by battery status if enabled